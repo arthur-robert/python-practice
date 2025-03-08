abc5184939f01bd30e88edc83505407af8394d37
Robert Arthur <robertarthur318@gmail.com>
2025-03-08 19:40:52 +0000

    added file

---
+++ pro.py
@@ -0,0 +1,8 @@
+name = ("Please enter your name here :")
+age = int(input("Please enter your age :"))
+
+if age >= 18:
+    print(f"You are eligible to drive {name}")
+
+else:
+    print(f"You are not eligible to drive {name} ")
