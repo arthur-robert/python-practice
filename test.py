6105936e1b2106c21e34f16dbb9b830bf4c59742
Robert Arthur <robertarthur318@gmail.com>
2025-03-12 19:30:45 +0000

    added python file

---
+++ test.py
@@ -0,0 +1,12 @@
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+car1 = Car("Toyota", "Camry")
+car1 = Car("Toyota", "Camry")
+print(car1.brand, end = " ") 
+print(car1.model)
+
---
+++ work.py
@@ -0,0 +1,5 @@
+user_data = {"name": "Alice", "age": 27, "city": "Cape Coast" }
+
+removed_age = user_data.pop("age")
+print(removed_age, end=" ")
+print(user_data)
